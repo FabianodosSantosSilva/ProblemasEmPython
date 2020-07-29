d391d9835ccdf69c3065f3b30adc1ff96c2c16b1
Fabiano dos Santos <ftrayder@gmail.com>
2020-07-29 18:37:05 -0300

    Problema cocluido.

---
+++ problema0002.py
@@ -0,0 +1,6 @@
+''' Crie um programa que leia dois valores e mostre a soma entre eles. (curso de Python - Gustavo Guanabara)'''
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro: '))
+soma = n1 + n2
+print(f'A soma entre {n1} e {n2} é {soma}.')
